db0118e8306082cd749d8122333e315a64096b7b
b159732000 <b159732000@users.noreply.github.com>
2019-06-03 19:29:09 +0800

    全景鸟瞰的icon开始加了

--- src/components/AerialView/AerialView.jsx
+++ src/components/AerialView/AerialView.jsx
@@ -11,6 +11,8 @@ import AreaMainPano from "../../images/AerialView/Area/AreaMainPano.jpg";
 class AerialView extends React.Component {
     constructor(props) {
         super(props);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onTouchMove = this.onTouchMove.bind(this);
         this.state = {
             currentPano: "AreaMainPano",   //現在渲染的全景
             currentPanoImg: AreaMainPano,    //現在渲染的全景圖片檔案
@@ -18,13 +20,43 @@ class AerialView extends React.Component {
             secondMenuIsActive: false,
             currentFirstMenu: "areaView",
             leftTopMenuActiveItem: "mall",
+            allInPanoXY: {
+                mainProjectIcon: {
+                    pitch: -16,
+                    yaw: 120,
+                }
+            }
         }
     }
 
+    onMouseMove() {
+        this.updatePointDirectionRotate();
+    }
+    onTouchMove() {
+        this.updatePointDirectionRotate();
+    }
+
+    // 更新小地圖視角方向
+    updatePointDirectionRotate() {
+        let pitch = this.Pannellum.panorama.getPitch();
+        let yaw = this.Pannellum.panorama.getYaw();
+        let fov = this.Pannellum.panorama.getHfov();
+        console.log("Pitch: " + pitch);
+        console.log("Yaw: " + yaw);
+        console.log("Fov: " + fov);
+    }
+
     componentDidMount() {
+        document.addEventListener('mousemove', this.onMouseMove);
+        document.addEventListener('touchmove', this.onTouchMove);
         setTimeout(() => this.setState({
             firstMenuIsActive: true,
         }), 400);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousemove', this.onMouseMove);
+        document.removeEventListener('touchmove', this.onTouchMove);
     }
 
     handleMenuClick(newPanoImg) {
@@ -71,23 +103,95 @@ class AerialView extends React.Component {
         return (
             <div className="aerialViewContainer">
 
+                {/* 全景圖中心點，用來找hotspot點位置 */}
+                <div className="centerPoint"></div>
+
                 <Pannellum
                     width="100%"
                     height="100%"
                     image={this.state.currentPanoImg}
                     pitch={10}
-                    yaw={-22 /* 起始水平視角位置 */}
-                    pitch={-32 /* 起始垂直視角位置 */}
-                    hfov={80 /* 起始視角縮放 */}
+                    yaw={121 /* 起始水平視角位置 */}
+                    pitch={-9.9 /* 起始垂直視角位置 */}
+                    hfov={60 /* 起始視角縮放 */}
                     maxHfov={90}
-                    minHfov={60}
+                    minHfov={50}
                     haov={360 /* 圖片水平寬度 */}
                     vaov={180 /* 圖片垂直寬度 */}
                     compass={false}
                     showZoomCtrl={false}
                     showFullscreenCtrl={false}
                     autoLoad
+                    ref={self => this.Pannellum = self}
                 >
+
+                    {/* 全景圖中專案本體Icon */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={this.state.allInPanoXY.mainProjectIcon.pitch}
+                        yaw={this.state.allInPanoXY.mainProjectIcon.yaw}
+                        text="This is texting"
+                        cssClass="mainHotspot"
+                    />
+
+                    {/* 公共 */}
+                    {/* 上城区酒店 */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={-14}
+                        yaw={42.7}
+                        text="This is texting"
+                        cssClass="onTownHotelHotspot"
+                    />
+                    {/* 诺沃尔酒店 */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={-9.4}
+                        yaw={46.1}
+                        text="This is texting"
+                        cssClass="noWallHotelHotspot"
+                    />
+                    {/* 斯特兰德酒店 */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={-3.3}
+                        yaw={54.6}
+                        text="This is texting"
+                        cssClass="stelandhotelHotspot"
+                    />
+                    {/* Yoma银行 */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={-2}
+                        yaw={84.6}
+                        text="This is texting"
+                        cssClass="yomaBankHotelHotspot"
+                    />
+                    {/* AGD银行 */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={-3.2}
+                        yaw={90.7}
+                        text="This is texting"
+                        cssClass="AGDBankHotelHotspot"
+                    />
+                    {/* 达给达警局 */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={-6.4}
+                        yaw={138.8}
+                        text="This is texting"
+                        cssClass="daGeDaPoliceHotspot"
+                    />
+                    {/* AYA银行 */}
+                    <Pannellum.Hotspot
+                        type="custom"
+                        pitch={-6.9}
+                        yaw={149.5}
+                        text="This is texting"
+                        cssClass="AYABankHotspot"
+                    />
+
                 </Pannellum>
 
                 <div className="leftTopMenu">
